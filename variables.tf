bfc717384b6a414278206acca4707af5c941e231
cloud_user-at-426598615503 <cloud_user@4265-9861-5503>
2023-03-14 06:25:18 -0700

    Terraform Modules

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-069a823dc1dc5c2c8"
+}
+variable "amiID" {
+	default="ami-0521a1ab6cb98215d"
+}
+variable "keyPair" {
+	default="key"
+}
+variable "keyPair1" {
+	default="key.pem"
+}
+
+
+
+
